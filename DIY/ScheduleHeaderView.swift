e3ecce4b6afdc2899af80c4871a7b08789028c7c
hien.bui <hien.bd@seesaa.vn>
2020-06-26 15:38:34 +0700

    scale foreground image view

--- DIY/ScheduleHeaderView.swift
+++ DIY/ScheduleHeaderView.swift
@@ -13,19 +13,25 @@ class ScheduleHeaderView: UICollectionReusableView {
   @IBOutlet private weak var backgroundImageView: UIView!
     @IBOutlet private weak var backgroundImageViewHeightLayoutConstraint: NSLayoutConstraint!
   @IBOutlet private weak var foregroundImageView: UIView!
+    @IBOutlet private weak var foregroundImageViewHeightLayoutConstraint: NSLayoutConstraint!
   
     private var backgroundImageViewHeight: CGFloat = 0
+    private var foregroundImageViewHeight: CGFloat = 0
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         backgroundImageViewHeight = backgroundImageView.bounds.height
+        foregroundImageViewHeight = foregroundImageView.bounds.height
     }
     
     override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
         super.apply(layoutAttributes)
         
         let attributes = layoutAttributes as! DIYLayoutAttributes
+        // Nhỏ vào
         backgroundImageViewHeightLayoutConstraint.constant = backgroundImageViewHeight - attributes.deltaY
+        // To ra
+        foregroundImageViewHeightLayoutConstraint.constant = foregroundImageViewHeight + attributes.deltaY
     }
 }
